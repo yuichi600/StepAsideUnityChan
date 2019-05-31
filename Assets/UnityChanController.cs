cd70ee6ee157acf131631b5faa056387ad24b2f2
Murata-Optiplex7040 <y.murata@logic-design.jp>
2019-05-31 23:45:29 +0900

    [ update  ] 7.3 UnityChanが消えてしまった。。

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -16,6 +16,11 @@ public class UnityChanController : MonoBehaviour {
 	private float movableRange = 3.4f;
 	//ジャンプするための力（追加）
 	private float upForce = 500.0f;
+	 //動きを減速させる係数（追加）
+	 private float coefficient = 0.95f;
+	//ゲーム終了の判定（追加）
+	private bool isEnd = false;
+
 
 
 	// Use this for initialization
@@ -26,15 +31,21 @@ public class UnityChanController : MonoBehaviour {
 		//this.myAnimator.SetFloat ("Speed", 1);
 		//Rigidbodyコンポーネントを取得（追加）
 		this.myRigidbody = GetComponent<Rigidbody>();
-		Debug.Log("TEST ="+myRigidbody);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	
+
+		//ゲーム終了ならUnityちゃんの動きを減衰する（追加）
+		if (this.isEnd) {
+			this.forwardForce *= this.coefficient;
+			this.turnForce *= this.coefficient;
+			this.upForce *= this.coefficient;
+			this.myAnimator.speed *= this.coefficient;
+		}
+			
 		//Unityちゃんに前方向の力を加える（追加）
-		 this.myRigidbody.AddForce (this.transform.forward * this.forwardForce);
+		this.myRigidbody.AddForce (this.transform.forward * this.forwardForce);
 		 //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
 		 if (Input.GetKey (KeyCode.LeftArrow)  && -this.movableRange < this.transform.position.x) {
 			  //左に移動（追加）
@@ -55,6 +66,25 @@ public class UnityChanController : MonoBehaviour {
 			this.myAnimator.SetBool ("Jump", true);
 			//Unityちゃんに上方向の力を加える（追加）
 			this.myRigidbody.AddForce (this.transform.up * this.upForce);
+		}
+
+	}
+	//トリガーモードで他のオブジェクトと接触した場合の処理（追加）
+	void OnTriggerEnter(Collider other) {
+		//障害物に衝突した場合（追加）
+		if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag") {
+			this.isEnd = true;
+		}
+		//ゴール地点に到達した場合（追加）
+		if (other.gameObject.tag == "GoalTag") {
+			this.isEnd = true;
+		}
+		//コインに衝突した場合（追加）
+		if (other.gameObject.tag == "CoinTag") {
+			//パーティクルを再生（追加）
+			GetComponent<ParticleSystem> ().Play ();
+			//接触したコインのオブジェクトを破棄（追加）
+			Destroy (other.gameObject);
 		}
 	}
 }
